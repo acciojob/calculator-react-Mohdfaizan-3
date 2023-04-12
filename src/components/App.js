6a4aae23f7d189f2f5d4a0651da72fa3c1d01c06
Mohd Faizan <faizan.mohd09@gmail.com>
2023-04-12 21:10:43 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,49 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from "react";
 
+const App = () => {
+  const [text, setText] = useState("");
+
+  const handleClick = (event) => {
+    setText(text + event.target.innerText);
+  };
+
+  const answer = () => {
+    try {
+      let ans = eval(text);
+      setText(ans);
+    } catch (error) {
+      setText("Invalid input");
+    }
+  };
+
+  const handleClear = () => {
+    setText("");
+  };
+
+  return (
+    <>
+      <input type="text" className=" calculator-input" value={text} readOnly />
+      <div className="buttons-container">
+        <button onClick={handleClear}>C</button>
+        <button onClick={handleClick}>/</button>
+        <button onClick={handleClick}>*</button>
+        <button onClick={handleClick}>-</button>
+        <button onClick={handleClick}>7</button>
+        <button onClick={handleClick}>8</button>
+        <button onClick={handleClick}>9</button>
+        <button onClick={handleClick}>+</button>
+        <button onClick={handleClick}>4</button>
+        <button onClick={handleClick}>5</button>
+        <button onClick={handleClick}>6</button>
+        <button onClick={handleClick}>1</button>
+        <button onClick={handleClick}>2</button>
+        <button onClick={handleClick}>3</button>
+        <button onClick={answer}>=</button>
+        <button onClick={handleClick}>0</button>
+        <button onClick={handleClick}>.</button>
+      </div>
+    </>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import "./styles/App.css"
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 
+ReactDOM.render(<App />, document.getElementById('root'));
 
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App/>);  <- react 18
